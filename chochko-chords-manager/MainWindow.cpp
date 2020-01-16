56c187bf732e8edbd37a305a2f9f204a4e2cd7a2
RedCuckoo <momotenko.yuriy@gmail.com>
2020-01-16 04:23:52 +0200

    Added some web, you can navigate the website, homepage

--- chochko-chords-manager/MainWindow.cpp
+++ chochko-chords-manager/MainWindow.cpp
@@ -13,7 +13,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
 	}
 
 	QFileSystemModel* playlistsModel = new QFileSystemModel;
-	qDebug() << QDir::currentPath();
+	//qDebug() << QDir::currentPath();
 	playlistsModel->setRootPath(QDir::currentPath());
 	
 	ui.treeView->setModel(playlistsModel);
@@ -25,14 +25,53 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
 
 	ui.treeView->setHeaderHidden(true);
 	ui.treeView->setRootIndex(playlistsModel->index("Playlists"));
+
+	(topStatusBar = new QStatusBar(this))->addWidget(progressBar = new QProgressBar(this));
+	QVBoxLayout* verticalLayout = new QVBoxLayout(this);
+	verticalLayout->addWidget(topStatusBar);
+	verticalLayout->addWidget(ui.statusBar);
+	ui.verticalLayout->addLayout(verticalLayout);
+	progressBar->hide();
+
+	ui.statusBar->addWidget(label = new QLabel(this));
+	label->setText("");
+
+	ui.actionNew_Song->setDisabled(true);
+
+
+	validatedSongUrl = QRegExp("^(https\\:\\/\\/(www\\.)?mychords\\.net\\/)([\\da-z-_\\.]+\\/)*([\\da-z-_\\.]+\\.html)$");
+
 }
 
 MainWindow::~MainWindow(){
 
 }
 
+//redo
 void MainWindow::on_actionNew_Song_triggered() {
+	
+	ui.webEngineView->page()->toHtml([this](const QString& a) {
+		QString title = ui.webEngineView->title();
+		QModelIndexList selectedIndexes = ui.treeView->selectionModel()->selectedIndexes();
+		if (selectedIndexes.size() > 0) {
+			QString playlistName = selectedIndexes.at(0).data().toString();
+			QFile* newSong = new QFile();
 
+			newSong->setFileName(QDir::currentPath() + "/Playlists/" + playlistName + '/' + title + ".html");
+			newSong->open(QIODevice::WriteOnly);
+			QTextStream outputHTML(newSong);
+			outputHTML << a;
+			newSong->close();
+
+			
+		
+		}
+
+	});
+
+	QFile testQFile(QDir::currentPath() + "/Playlists/" + ui.treeView->selectionModel()->selectedIndexes().at(0).data().toString() + '/' + ui.webEngineView->title() + ".html");
+	testQFile.open(QIODevice::ReadOnly);
+	ui.webEngineView->setHtml(QTextStream(&testQFile).readAll());
 }
 
 void MainWindow::on_actionNew_Playlist_triggered() {
@@ -54,3 +93,30 @@ void MainWindow::on_actionDelete_selection_triggered() {
 		}
 	}
 }
+
+void MainWindow::on_actionHome_page_triggered() {
+	ui.webEngineView->load(QUrl("https://mychords.net/"));
+}
+
+void MainWindow::on_webEngineView_loadFinished() {
+	progressBar->hide();
+}
+
+void MainWindow::on_webEngineView_loadProgress(int progress) {
+	progressBar->show();
+	progressBar->setValue(progress);
+}
+
+void MainWindow::on_webEngineView_urlChanged(QUrl newUrl) {
+	QString currentUrl = newUrl.toString();
+
+	label->setText(currentUrl);
+	
+	if (currentUrl.contains(validatedSongUrl)){
+		ui.actionNew_Song->setEnabled(true);
+	}
+	else {
+		ui.actionNew_Song->setDisabled(true);
+	}
+}
+
--- chochko-chords-manager/MainWindow.h
+++ chochko-chords-manager/MainWindow.h
@@ -3,6 +3,10 @@
 #include <QMainWindow>
 #include <QFile>
 #include "ui_MainWindow.h"
+#include <QProgressBar>
+#include <QStatusBar>
+#include <QLabel>
+#include <QRegExp>
 
 class MainWindow : public QMainWindow{
 	Q_OBJECT
@@ -13,9 +17,20 @@ public:
 
 private:
 	Ui::MainWindow ui;
+	QStatusBar* topStatusBar;
+	QProgressBar* progressBar;
+	QLabel* label;
+
+	QRegExp validatedSongUrl;
+	
 
 private slots:
 	void on_actionNew_Song_triggered();
 	void on_actionNew_Playlist_triggered();
 	void on_actionDelete_selection_triggered();
+	void on_actionHome_page_triggered();
+	void on_webEngineView_loadFinished();
+	void on_webEngineView_loadProgress(int);
+	void on_webEngineView_urlChanged(QUrl);
+
 };
