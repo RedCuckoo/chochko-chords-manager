99bf91b2af6671f73dff394e323c358c6668ef02
RedCuckoo <momotenko.yuriy@gmail.com>
2020-03-10 11:50:40 +0200

    Renamed visual studio project fix

--- chochko-chords-manager/GreetingMessageBox.cpp
+++
@@ -1,9 +0,0 @@
-#include "GreetingMessageBox.h"
-
-GreetingMessageBox::GreetingMessageBox(QWidget *parent) : QMessageBox(parent){
-	ui.setupUi(this);
-}
-
-GreetingMessageBox::~GreetingMessageBox(){
-
-}
